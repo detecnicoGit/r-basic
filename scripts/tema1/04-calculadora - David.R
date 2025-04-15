2c532e6839b3b1f3bae1cae8c4612333eed131f1
detecnicoGit <estudio.davidrengel@gmail.com>
2025-04-15 18:21:35 +0200

    Clase sección 4

--- scripts/tema1/04-calculadora - David.R
+++ scripts/tema1/04-calculadora - David.R
@@ -29,3 +29,24 @@ NaN
 1/0
 
 c(2^30, 2^(-15), 1, 2, 3/2)
+abs(-pi)
+factorial(7)
+factorial(4)
+factorial(-5)
+factorial(abs(-5))
+choose(5,3)
+log(2,32)
+log(32,2)
+choose(5,1)
+6^log(4,6)
+6^2
+
+#TRIGONOMETRÍA
+cos(60 * pi/180)
+sin(60 * pi/180)
+tan(pi)
+asin(0.8660254)
+asin(0.8660254)*180/pi
+
+#REDONDEOS
+
